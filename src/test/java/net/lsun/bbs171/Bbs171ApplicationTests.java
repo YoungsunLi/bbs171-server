5570334fd7247e0c644e40b3c229c0fcd18267df
YoungsunLi <lsun@live.cn>
2021-01-11 13:15:24 +0800

    init

---
+++ src/test/java/net/lsun/bbs171/Bbs171ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lsun.bbs171;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bbs171ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
