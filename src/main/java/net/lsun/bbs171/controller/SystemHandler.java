ad0c5452cafd68b3848364b781fda79262f11528
YoungsunLi <lsun@live.cn>
2021-05-05 23:36:41 +0800

    模块管理

--- src/main/java/net/lsun/bbs171/controller/SystemHandler.java
+++ src/main/java/net/lsun/bbs171/controller/SystemHandler.java
@@ -1,13 +1,16 @@
 package net.lsun.bbs171.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import net.lsun.bbs171.entity.Category;
 import net.lsun.bbs171.entity.SystemInfoDTO;
+import net.lsun.bbs171.entity.User;
 import net.lsun.bbs171.repository.SystemRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.lsun.bbs171.repository.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("system")
@@ -15,6 +18,9 @@ public class SystemHandler {
 
     @Resource
     private SystemRepository systemRepository;
+
+    @Resource
+    private UserRepository userRepository;
 
     /**
      * 获取社区运行状况
@@ -29,6 +35,114 @@ public class SystemHandler {
 
         json.put("success", true);
         json.put("data", systemInfo);
+
+        return json;
+    }
+
+    /**
+     * 获取管理员列表
+     *
+     * @return users
+     */
+    @GetMapping("get_admin_users")
+    public JSONObject getAdminUsers() {
+        JSONObject json = new JSONObject();
+
+        int authId = Integer.parseInt(SecurityContextHolder.getContext().getAuthentication().getName());
+        User dbUser = userRepository.findById(authId);
+
+        if (dbUser.getRole() != 100) {
+            json.put("success", false);
+            json.put("msg", "非法操作!");
+        } else {
+            List<User> users = systemRepository.getAdminUsers();
+
+            json.put("success", true);
+            json.put("data", users);
+        }
+
+        return json;
+    }
+
+    /**
+     * 添加板块(分类)
+     *
+     * @return users
+     */
+    @PostMapping("add_category")
+    public JSONObject addCategory(@RequestBody Category category) {
+        JSONObject json = new JSONObject();
+
+        int authId = Integer.parseInt(SecurityContextHolder.getContext().getAuthentication().getName());
+        User dbUser = userRepository.findById(authId);
+
+        if (dbUser.getRole() != 100) {
+            json.put("success", false);
+            json.put("msg", "非法操作!");
+        } else {
+            systemRepository.addCategory(category);
+
+            json.put("success", true);
+            json.put("msg", "添加成功!");
+        }
+
+        return json;
+    }
+
+    /**
+     * 删除板块(分类)
+     *
+     * @return users
+     */
+    @PostMapping("del_category")
+    public JSONObject delCategory(@RequestBody Category category) {
+        JSONObject json = new JSONObject();
+
+        int authId = Integer.parseInt(SecurityContextHolder.getContext().getAuthentication().getName());
+        User dbUser = userRepository.findById(authId);
+
+        if (dbUser.getRole() != 100) {
+            json.put("success", false);
+            json.put("msg", "非法操作!");
+        } else {
+            int count = systemRepository.getCategoryUseCount(category);
+
+            if (count > 0) {
+                json.put("success", false);
+                json.put("msg", "删除失败: 该板块已关联 " + count + " 个帖子!");
+            } else {
+                systemRepository.delCategory(category);
+
+                json.put("success", true);
+                json.put("msg", "删除成功!");
+            }
+        }
+
+        return json;
+    }
+
+    /**
+     * 修改板块(分类)
+     *
+     * @return users
+     */
+    @PostMapping("update_category")
+    public JSONObject updateCategory(@RequestBody Category category) {
+        JSONObject json = new JSONObject();
+
+        int authId = Integer.parseInt(SecurityContextHolder.getContext().getAuthentication().getName());
+        User dbUser = userRepository.findById(authId);
+
+        if (dbUser.getRole() != 100) {
+            json.put("success", false);
+            json.put("msg", "非法操作!");
+        } else {
+            systemRepository.updateCategory(category);
+
+            json.put("success", true);
+            json.put("msg", "修改成功!");
+
+        }
 
         return json;
     }
--- src/main/java/net/lsun/bbs171/repository/SystemRepository.java
+++ src/main/java/net/lsun/bbs171/repository/SystemRepository.java
@@ -1,10 +1,22 @@
 package net.lsun.bbs171.repository;
 
+import net.lsun.bbs171.entity.Category;
 import net.lsun.bbs171.entity.Level;
 import net.lsun.bbs171.entity.SystemInfoDTO;
+import net.lsun.bbs171.entity.User;
 
 import java.util.List;
 
 public interface SystemRepository {
     SystemInfoDTO getInfo();
+
+    List<User> getAdminUsers();
+
+    void addCategory(Category category);
+
+    void delCategory(Category category);
+
+    int getCategoryUseCount(Category category);
+
+    void updateCategory(Category category);
 }
